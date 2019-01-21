93ad8d054aac21364248a9d1b0f128a1a1aa5da3
Luca Mazzei <luk250788@gmail.com>
2019-01-21 16:44:40 +0100

    Implements download and upload file methods

---
+++ src/main/java/gDriveSynch/DriveClient.java
@@ -0,0 +1,80 @@
+package gDriveSynch;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.googleapis.media.MediaHttpDownloader;
+import com.google.api.client.googleapis.media.MediaHttpUploader;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.Drive.Files.Create;
+import com.google.api.services.drive.Drive.Files.Get;
+import com.google.api.services.drive.model.FileList;
+
+public class DriveClient {
+
+	private final JsonFactory jsonFactory;
+	private final HttpTransport httpTransport;
+	private final Credential credential;
+	private final List<String> scopes;
+	private final Drive drive;
+
+	public DriveClient(JsonFactory jsonFactory, HttpTransport httpTransport, Credential credential, String appName,
+			List<String> scopes) {
+		super();
+		this.jsonFactory = jsonFactory;
+		this.httpTransport = httpTransport;
+		this.credential = credential;
+		this.scopes = scopes;
+		this.drive = new Drive.Builder(httpTransport, jsonFactory, credential).setApplicationName(appName).build();
+	}
+
+	public FileList getFileList() {
+		return null;
+	}
+
+	public com.google.api.services.drive.model.File uploadFile(File file, String mimeType) throws GoogleJsonResponseException, IOException {
+
+		com.google.api.services.drive.model.File fileMetadata = new com.google.api.services.drive.model.File();
+
+		fileMetadata.setName(file.getName());
+		FileContent fileContent = new FileContent(mimeType, file);
+		Create create = drive.files().create(fileMetadata, fileContent);
+		GenericUrl genericUrl = create.buildHttpRequestUrl();
+		MediaHttpUploader uploader = create.getMediaHttpUploader();
+		uploader.setProgressListener(new CustomProgressListener());
+		HttpResponse response = uploader.upload(genericUrl);
+		if(!response.isSuccessStatusCode()){
+			throw GoogleJsonResponseException.from(jsonFactory, response);
+		}
+		
+		try (InputStream in = response.getContent()){
+			return jsonFactory.fromInputStream(in, com.google.api.services.drive.model.File.class);
+		}
+	}
+
+	public File downloadFile(String id, File outputDir) throws IOException {
+		
+		Get get = drive.files().get(id);
+		com.google.api.services.drive.model.File metadata = get.setFields("name").execute();
+		File output = new File(outputDir, metadata.getName());
+		
+		MediaHttpDownloader downloader = get.getMediaHttpDownloader();
+//		downloader.setProgressListener(progressListener);
+		try(OutputStream out = new FileOutputStream(output)){
+			downloader.download(get.buildHttpRequestUrl(), out);
+		}
+		
+		return output;
+	}
+}
---
+++ src/test/java/gDriveSynch/DriveClientTest.java
@@ -0,0 +1,91 @@
+package gDriveSynch;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.activation.MimeType;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.mortbay.jetty.MimeTypes;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.DriveScopes;
+import com.google.common.base.Joiner;
+
+public class DriveClientTest {
+
+	DriveClient sut;
+
+	private static final String APP_NAME = "g-drive-synch";
+	
+	@Rule public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+	@Before
+	public void setup() throws GeneralSecurityException, IOException {
+		JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+		HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+		List<String> scopes = Arrays.asList(DriveScopes.DRIVE);
+		sut = new DriveClient(jsonFactory, httpTransport,
+				getCredential(httpTransport, jsonFactory, scopes, new File("."),
+						GoogleClientSecrets.load(jsonFactory,
+								new InputStreamReader(
+										DriveClientTest.class.getResourceAsStream("/client_secret.json")))),
+				APP_NAME, scopes);
+	}
+
+	private Credential getCredential(HttpTransport httpTransport, JsonFactory jsonFactory, List<String> scopes,
+			File dataStorePath, GoogleClientSecrets clientSecrets) throws IOException {
+		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.//
+				Builder(httpTransport, jsonFactory, clientSecrets, scopes)//
+						.setDataStoreFactory(new FileDataStoreFactory(dataStorePath))//
+						.build();
+		LocalServerReceiver receiver = new LocalServerReceiver.Builder().setHost("[::1]").setPort(8888).build();
+		return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+	}
+	
+	@Test
+	public void uploadTest() throws Exception {
+		
+		File test = writeOnFile(temporaryFolder.newFile(), "TEST");
+		
+		com.google.api.services.drive.model.File actual = sut.uploadFile(test, MimeTypes.TEXT_PLAIN);
+		Assert.assertNotNull(actual);
+		System.out.println("File id " + actual.getId() + " md5 sum " + actual.getMd5Checksum());
+	}
+	
+	@Test
+	public void downloadFile() throws Exception {
+		
+		File actual = sut.downloadFile("1WUvNf8376qRCtU1yf7bVvmnqvHATY-Qg", temporaryFolder.newFolder());
+		
+		Assert.assertTrue(actual.isFile());
+		System.out.println(Joiner.on(System.lineSeparator()).join(Files.readAllLines(actual.toPath())));
+	}
+	
+	private File writeOnFile(File file, String content) throws IOException {
+		try(FileWriter writer = new FileWriter(file)){
+			writer.write(content);
+		}
+		return file;
+	}
+
+}
